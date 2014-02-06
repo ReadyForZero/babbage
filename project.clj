19db1930595c1f2aa4ab3aade42689b0d69c981a
ben wolfson <wolfson@readyforzero.com>
2014-02-05 17:41:35 -0800

    de-snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject readyforzero/babbage "1.1.0-SNAPSHOT"
+(defproject readyforzero/babbage "1.1.0"
   :min-lein-version "2.0.0"
   :dependencies [[expectations "1.4.18"]
                  [org.clojure/clojure "1.5.1"]
